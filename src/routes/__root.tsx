42afffae127ad28205b91febc937600ac0f37bf4
haikalgakbar <haikalgakbar@gmail.com>
2024-11-10 13:56:53 +0700

    update navbar behaviour

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,10 @@
 import { createDailyTransaction } from "@/libs/util";
-import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+import {
+  createRootRoute,
+  Link,
+  Outlet,
+  useLocation,
+} from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import { CircleCheckBig, Pill, History } from "lucide-react";
 import { useEffect } from "react";
@@ -9,6 +14,9 @@ export const Route = createRootRoute({
 });
 
 function Root() {
+  const location = useLocation();
+  const currentPath = location.pathname;
+
   useEffect(() => {
     async function test() {
       createDailyTransaction();
@@ -21,9 +29,11 @@ function Root() {
       <nav className="fixed bottom-0 flex w-full gap-2 bg-[#171717]/80 p-4 backdrop-blur-xl backdrop-brightness-50">
         <Link
           to="/"
-          className="flex w-full flex-col items-center justify-center gap-1 overflow-hidden rounded-xl text-[#f5f5f5]"
+          className="group flex w-full flex-col items-center justify-center gap-1 overflow-hidden rounded-xl text-[#f5f5f5]"
         >
-          <span className="rounded-full bg-[#F5F5F5] px-4 py-1 text-[#171717]">
+          <span
+            className={`rounded-full px-4 py-1 ${currentPath === "/" ? "bg-[#F5F5F5] text-[#171717]" : "text-[#D4D4D4] group-hover:bg-[#404040]"}`}
+          >
             <CircleCheckBig size={20} />
           </span>
           <span>Check-in</span>
@@ -32,7 +42,9 @@ function Root() {
           to="/medicine"
           className="group flex w-full flex-col items-center justify-center gap-1 overflow-hidden rounded-xl text-[#f5f5f5]"
         >
-          <span className="rounded-full px-4 py-1 text-[#D4D4D4] group-hover:bg-[#404040]">
+          <span
+            className={`rounded-full px-4 py-1 ${currentPath === "/medicine" ? "bg-[#F5F5F5] text-[#171717]" : "text-[#D4D4D4] group-hover:bg-[#404040]"}`}
+          >
             <Pill size={20} />
           </span>
           <span>Medicine</span>
@@ -41,14 +53,16 @@ function Root() {
           to="/history"
           className="group flex w-full flex-col items-center justify-center gap-1 overflow-hidden rounded-xl text-[#f5f5f5]"
         >
-          <span className="rounded-full px-4 py-1 text-[#D4D4D4] group-hover:bg-[#404040]">
+          <span
+            className={`rounded-full px-4 py-1 ${currentPath === "/history" ? "bg-[#F5F5F5] text-[#171717]" : "text-[#D4D4D4] group-hover:bg-[#404040]"}`}
+          >
             <History size={20} />
           </span>
           <span>History</span>
         </Link>
       </nav>
       <Outlet />
-      {/* <TanStackRouterDevtools /> */}
+      <TanStackRouterDevtools />
     </>
   );
 }
