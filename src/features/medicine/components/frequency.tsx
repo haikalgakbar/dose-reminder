a9d8992d1f1dd59bb22f181db184981c454ceb0c
haikalgakbar <haikalgakbar@gmail.com>
2025-01-02 16:04:55 +0700

    refactor: enhance frequency component with improved dialog styling and close functionality

--- src/features/medicine/components/frequency.tsx
+++ src/features/medicine/components/frequency.tsx
@@ -2,6 +2,7 @@ import { useMediaQuery } from "@uidotdev/usehooks";
 import { useState } from "react";
 import {
   Dialog,
+  DialogClose,
   DialogContent,
   DialogDescription,
   DialogHeader,
@@ -143,13 +144,22 @@ function FrequencyForm({
             <ChevronRight size={20} />
           </Button>
         </DialogTrigger>
-        <DialogContent className="sm:max-w-[425px]">
-          <DialogHeader>
-            <DialogTitle>Edit profile</DialogTitle>
-            <DialogDescription>
+        <DialogContent className="max-w-96 rounded-xl border-0 bg-neutral-900 text-neutral-200 focus:outline-none focus:ring-0">
+          <DialogHeader className="flex-row items-center gap-2">
+            <DialogTitle className="w-full">Edit frequency</DialogTitle>
+            <DialogDescription className="sr-only">
               Make changes to your profile here. Click save when you're done.
             </DialogDescription>
+            <DialogClose className="mt-0 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
+              <X className="h-4 w-4" />
+              <span className="sr-only">Close</span>
+            </DialogClose>
           </DialogHeader>
+          <EditFrequencyForm
+            setOpen={setOpen}
+            medicine={medicine}
+            setMedicine={setMedicine}
+          />
         </DialogContent>
       </Dialog>
     );
